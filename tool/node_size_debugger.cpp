7884b25ac5e6391642205d9f7aa7b04afa6597fc
Jonathan Müller <jonathanmueller.dev@gmail.com>
2015-07-16 11:17:11 +0200

    * implemented basic command line options

--- tool/node_size_debugger.cpp
+++ tool/node_size_debugger.cpp
@@ -2,6 +2,7 @@
 // This file is subject to the license terms in the LICENSE file
 // found in the top-level directory of this distribution.
 
+#include <cstring>
 #include <iostream>
 #include <string>
 
@@ -42,7 +43,47 @@ void serialize(const Serializer &serializer)
     serialize_impl(serializer, debuggers{});
 }
 
-int main()
+const char* exe_name = "foonathan_memory_node_size_debugger";
+std::string exe_spaces(std::strlen(exe_name), ' ');
+
+void print_help(std::ostream &out)
 {
-    serialize(simple_serializer{std::cout});
+    out << "Usage: " << exe_name << " [--version][--help]\n";
+    out << "       "  << exe_spaces << " [--simple]\n";
+    out << "Obtains information about the internal node sizes of the STL containers.\n";
+    out << '\n';
+    out << "   --simple\tprints node sizes in the form 'alignment=base-node-size'\n";
+    out << "   --help\tdisplay this help and exit\n";
+    out << "   --version\toutput version information and exit\n";
+    out << '\n';
+    out << "The base node size is the size of the node without the storage for the value type.\n"
+        << "Add 'sizeof(value_type)' to the base node size for the appropriate alignment to get the whole size.\n";
+    out << "With no options prints base node sizes of all containers in a simple manner.\n";
+}
+
+void print_version(std::ostream &out)
+{
+    out << exe_name << " version " << VERSION << '\n';
+}
+
+int print_invalid_option(std::ostream &out, const char *option)
+{
+    out << exe_name << ": invalid option -- '";
+    while (*option == '-')
+        ++option;
+    out << option << "'\n";
+    out << "Try '" << exe_name << " --help' for more information.\n";
+    return 2;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1 || argv[1] == std::string("--simple"))
+        serialize(simple_serializer{std::cout});
+    else if (argv[1] == std::string("--help"))
+        print_help(std::cout);
+    else if (argv[1] == std::string("--version"))
+        print_version(std::cout);
+    else
+        return print_invalid_option(std::cout, argv[1]);
 }
