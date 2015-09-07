0798e6e49de2c47ca9dad645b21edfd35b48b2a4
Jonathan Müller <jonathanmueller.dev@gmail.com>
2015-09-07 22:15:53 +0200

    * fixed issue with generation of container_node_sizes.hpp by removing container_node_sizes.hpp.in and --append option and using the "generate file"-version of add_custom_command * used dummy interface library for running compatibility tests so that information whether or not hosted will be available prior to deciding whether or not to build tests

--- cmake/compatibility.cmake
+++ cmake/compatibility.cmake
@@ -10,11 +10,18 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/comp/comp_base.cmake)
 # download in source for convenience
 set(_foonathan_comp_dest_dir ${CMAKE_CURRENT_SOURCE_DIR}/cmake/comp)
 
-function(_foonathan_use_comp target)
-    comp_target_features(${target} PUBLIC
+# dummy library running the required tests
+add_library(_foonathan_comp_runner INTERFACE)
+comp_target_features(_foonathan_comp_runner INTERFACE
         cpp11_lang/alignof cpp11_lang/constexpr cpp11_lang/noexcept cpp11_lang/thread_local
         cpp11_lib/max_align_t cpp11_lib/get_new_handler
         env/exception_support env/hosted_implementation env/threading_support
         PREFIX "FOONATHAN_" NAMESPACE "foonathan_memory_comp"
-        CMAKE_PATH "${_foonathan_comp_dest_dir}")
+        CMAKE_PATH "${_foonathan_comp_dest_dir}"
+        NOFLAGS)
+
+function(_foonathan_use_comp target)
+    # just activate C++11
+    comp_target_features(${target} PRIVATE CPP11)
+    target_link_libraries(${target} PUBLIC _foonathan_comp_runner)
 endfunction()
