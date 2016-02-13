be3ef56ca9ef8ea5a29e1192e6474aac1ea1fdae
Jonathan Müller <jonathanmueller.dev@gmail.com>
2016-02-13 11:55:35 +0100

    Add foonathan_memory-config.cmake file that allows find_package()

---
+++ cmake/foonathan_memory-config.cmake
@@ -0,0 +1,8 @@
+# Copyright (C) 2015-2016 Jonathan Müller <jonathanmueller.dev@gmail.com>
+# This file is subject to the license terms in the LICENSE file
+# found in the top-level directory of this distribution.
+
+# package configuration file
+
+get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(${SELF_DIR}/foonathan_memory.cmake)
