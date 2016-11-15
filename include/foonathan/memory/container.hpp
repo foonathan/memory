21cb1fded3f00fdc7ae5adc1a8ed35b77cc928f3
Jonathan Müller <jonathanmueller.dev@gmail.com>
2016-11-15 18:25:22 +0100

    [adapter] Add string container alias

--- include/foonathan/memory/container.hpp
+++ include/foonathan/memory/container.hpp
@@ -24,6 +24,7 @@
 #include <queue>
 #include <set>
 #include <stack>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -118,6 +119,12 @@ namespace foonathan
         template <typename T, class RawAllocator, class Mutex = default_mutex>
         FOONATHAN_ALIAS_TEMPLATE(priority_queue,
                                  std::priority_queue<T, deque<T, RawAllocator, Mutex>>);
+
+        /// \copydoc vector
+        template <class RawAllocator, class Mutex = default_mutex>
+        FOONATHAN_ALIAS_TEMPLATE(string,
+                                 std::basic_string<char, std::char_traits<char>,
+                                                   std_allocator<char, RawAllocator, Mutex>>);
         /// @}
 
         /// @{
