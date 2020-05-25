18ced54f093a13a8f7f97a5f4fe42506503f6967
Alberto Quattrini Li <alberto.quattrinili@gmail.com>
2020-05-24 22:10:08 -0400

    Use of VirtualAlloc compatible also with UWP
    VirtualAlloc to VirtualAllocFromApp so that it is compatible for both Windows Desktop and UWP (https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualallocfromapp).

--- src/virtual_memory.cpp
+++ src/virtual_memory.cpp
@@ -37,7 +37,7 @@ const std::size_t foonathan::memory::virtual_memory_page_size = get_page_size();
 void* foonathan::memory::virtual_memory_reserve(std::size_t no_pages) noexcept
 {
     auto pages =
-        VirtualAlloc(nullptr, no_pages * virtual_memory_page_size, MEM_RESERVE, PAGE_READWRITE);
+        VirtualAllocFromApp(nullptr, no_pages * virtual_memory_page_size, MEM_RESERVE, PAGE_READWRITE);
     return pages;
 }
 
@@ -51,7 +51,7 @@ void* foonathan::memory::virtual_memory_commit(void*       memory,
                                                std::size_t no_pages) noexcept
 {
     auto region =
-        VirtualAlloc(memory, no_pages * virtual_memory_page_size, MEM_COMMIT, PAGE_READWRITE);
+        VirtualAllocFromApp(memory, no_pages * virtual_memory_page_size, MEM_COMMIT, PAGE_READWRITE);
     if (!region)
         return nullptr;
     FOONATHAN_MEMORY_ASSERT(region == memory);
