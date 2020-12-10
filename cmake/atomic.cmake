08b994798cd60edbd30a2f623149027f43a618b6
Timo Röhling <timo@gaussglocke.de>
2020-12-10 13:22:52 +0100

    Add support for older compilers

--- cmake/atomic.cmake
+++ cmake/atomic.cmake
@@ -1,6 +1,6 @@
 # check for atomic library, which is needed on some architectures
 include(CheckCXXSourceCompiles)
-function(check_working_cxx_atomic varname)
+function(check_cxx_atomic_compiles varname)
 	check_cxx_source_compiles("
 	#include <atomic>
 	std::atomic<bool> x;
@@ -8,6 +8,29 @@ function(check_working_cxx_atomic varname)
 		bool y = false;
 		return !x.compare_exchange_strong(y, true);
 	}" ${varname})
+endfunction()
+function(check_working_cxx_atomic varname)
+	check_cxx_atomic_compiles(${varname}_NOFLAG)
+	if(${varname}_NOFLAG)
+		set(${varname} TRUE PARENT_SCOPE)
+		return()
+	endif()
+	set(old_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+	list(APPEND CMAKE_REQUIRED_FLAGS "-std=c++11")
+	check_cxx_atomic_compiles(${varname}_STD11)
+	set(CMAKE_REQUIRED_FLAGS  "${old_CMAKE_REQUIRED_FLAGS}")
+	if(${varname}_STD11)
+		set(${varname} TRUE PARENT_SCOPE)
+		return()
+	endif()
+	list(APPEND CMAKE_REQUIRED_FLAGS "-std=c++0x")
+	check_cxx_atomic_compiles(${varname}_STD0X)
+	set(CMAKE_REQUIRED_FLAGS  "${old_CMAKE_REQUIRED_FLAGS}")
+	if(${varname}_STD0X)
+		set(${varname} TRUE PARENT_SCOPE)
+		return()
+	endif()
+	set(${varname} FALSE PARENT_SCOPE)
 endfunction()
 check_working_cxx_atomic(HAVE_CXX_ATOMIC)
 if(NOT HAVE_CXX_ATOMIC)
