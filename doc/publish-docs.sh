f191eb0258175ff0ea0305a2f9e5d8e0a691d76e
Jonathan Müller <git@foonathan.net>
2023-01-11 12:38:13 +0100

    Update publish-docs script

--- doc/publish-docs.sh
+++ doc/publish-docs.sh
@@ -2,9 +2,11 @@
 # builds documentation and publishes it
 # run in root of repository, assumes `git worktree add doc/html gh-pages`
 
+rm -r doc/html/*
 doxygen doc/Doxyfile
+echo "memory.foonathan.net" > doc/html/CNAME
 cd doc/html
 git add --all
-git commit -am"Update documentation"
+git commit -am"Update documentation" --amend
 git push --force origin gh-pages
 
